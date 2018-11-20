0f1e755cb1e088dfd0a4b8cb9ee8528f519c1c3a
Steven Fisher <steven.fisher22@gmail.com>
2018-11-20 13:28:10 -0600

    Fixed problem #4

--- Homework 14 Nov 2018/list-exercises.py
+++ Homework 14 Nov 2018/list-exercises.py
@@ -19,13 +19,13 @@
 
 
 # 4 EVEN NUMBERS
-#Q said to print it but this also removes the Odd numbers. Need better solution. 
 
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+# newNumbers = []
 # for i in numbers:
-#     if(i%2 != 0):
-#         numbers.remove(i)
-# print(numbers)
+#     if(i%2 == 0):
+#         newNumbers.append(i)
+# print(newNumbers)
 
 
 # 5 POSITIVE NUMBERS
